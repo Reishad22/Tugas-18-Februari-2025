fcf7a4e85d49685069c3437e5ac983b65969daed
Reishad22 <Reishad.vikesha@gmail.com>
2025-02-18 20:29:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <Body>
+        <form action="proses.php" method="post">
+            <div>
+            Nama: <br>
+                <input type="text" name="nama"/>
+            </div>
+            <div>
+            Alamat: <br>
+                <input type="text" name="alamat"/>
+            </div>
+            <div>
+            No. Telepon: <br>
+                <input type="text" name="phone"/>
+            </div>
+            <button type="submit"> Kirim </button>
+        </form>
+    </body>
+</html>
---
+++ proses.php
@@ -0,0 +1,9 @@
+<?php
+
+$nama = $_POST["nama"];
+$alamat = $_POST["alamat"];
+$phone = $_POST["phone"];
+
+echo $nama . '<br>';
+echo $alamat . '<br>';
+echo $phone;
